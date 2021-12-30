3ab9d39f1f28e30970b5641fb1b8bb78640df07d
Jrandle10 <91491549+Jrandle10@users.noreply.github.com>
2021-12-30 16:22:52 -0700

    add NavBar and CSS styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, NavLink } from 'react-router-dom';
 import Home from "./components/Home";
 import About from "./components/About";
 import SinglePost from "./components/SinglePost";
 import Post from "./components/Post";
 import Project from "./components/Project";
+import NavBar  from "./components/NavBar";
 
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Home />} path='/' /> 
-      <Route element={<About />} path='/about' />
-      <Route element={<SinglePost />} path='/post/:id' />
-      <Route element={<Post />} path='/post' />
-      <Route element={<Project />} path='/project' />
-    </Routes>
+    <>
+      <NavBar/>
+      <Routes>
+        <Route element={<Home />} path='/' /> 
+        <Route element={<About />} path='/about' />
+        <Route element={<SinglePost />} path='/post/:id' />
+        <Route element={<Post />} path='/post' />
+        <Route element={<Project />} path='/project' />
+      </Routes>
+    </>
   )
 }
 
