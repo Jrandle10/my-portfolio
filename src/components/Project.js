5c23de6f6b8a0142ce0265749c4657e0643479dc
Jrandle10 <91491549+Jrandle10@users.noreply.github.com>
2022-01-02 00:33:11 -0700

    update css

--- src/components/Project.js
+++ src/components/Project.js
@@ -19,7 +19,7 @@ export default function Project() {
   }, []);
 
   return (
-    <main className='bg-pink-100 min-h-screen p-12'>
+    <main className='bg-pink-100 min-h-screen min-w-screen p-12'>
       <section className='container mx-auto'>
         <h1 className='text-5xl flex justify-center cursive'>My Projects</h1>
         <h2 className='text-lg text-gray-600 flex justify-center mb-12'>
