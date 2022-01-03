93f8224fb85699effe040dab43ee2a3892db035e
Jrandle10 <91491549+Jrandle10@users.noreply.github.com>
2022-01-03 13:05:12 -0700

    update home page styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
       <img 
       src={image} 
       alt='laptop background' 
-      className='sm:absolute sm:object-cover sm:w-full sm:h-full absolute object-cover w-full h-full   ' 
+      className='sm:absolute sm:object-cover sm:w-full sm:h-full absolute object-cover w-full h-full overflow-x-hidden' 
       />
       <section 
         className='sm:relative sm:flex sm:justify-center sm:min-h-screen sm:pt-12 lg:pt-64 sm:px-8 flex justify-center relative '
